f85c545cd621370da284565d85f479050d182719
Steve Burton <stevevburton@gmail.com>
2016-11-14 14:07:59 -0800

    Finish madlibs lesson

--- HomeModule.cs
+++ HomeModule.cs
@@ -1,1 +1,32 @@
+using Nancy;
+using MadLib.Objects;
 
+namespace MadLib
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/form"] = _ => {
+        return View["form.cshtml"];
+      };
+
+      Get["/story"] = _ => {
+        MadLibVariables myMadLibVariables = new MadLibVariables();
+        myMadLibVariables.SetAdjective1(Request.Query["adjective1"]);
+        myMadLibVariables.SetAdjective2(Request.Query["adjective2"]);
+        myMadLibVariables.SetPerson(Request.Query["person"]);
+        myMadLibVariables.SetPluralNoun(Request.Query["pluralNoun"]);
+        myMadLibVariables.SetNoun1(Request.Query["noun1"]);
+        myMadLibVariables.SetNoun2(Request.Query["noun2"]);
+        myMadLibVariables.SetVerb(Request.Query["verb"]);
+        myMadLibVariables.SetVegetable(Request.Query["vegetable"]);
+        myMadLibVariables.SetColor(Request.Query["color"]);
+        myMadLibVariables.SetNoun3(Request.Query["noun3"]);
+        return View["story.cshtml", myMadLibVariables];
+      };
+    }
+  }
+}
+
+// "Nancy": "1.3.0",
---
+++ Objects/MadLibVariables.cs
@@ -0,0 +1,112 @@
+namespace MadLib.Objects
+{
+  public class MadLibVariables
+  {
+
+    private string _adjective1;
+    private string _adjective2;
+    private string _person;
+    private string _pluralNoun;
+    private string _noun1;
+    private string _noun2;
+    private string _verb;
+    private string _vegetable;
+    private string _color;
+    private string _noun3;
+
+    public void SetAdjective1(string newAdjective)
+    {
+      _adjective1 =  newAdjective;
+    }
+    public string GetAdjective1()
+    {
+      return _adjective1;
+    }
+
+    public void SetAdjective2(string newerAdjective)
+    {
+      _adjective2 =  newerAdjective;
+    }
+    public string GetAdjective2()
+    {
+      return _adjective2;
+    }
+
+    public void SetPerson(string newPerson)
+    {
+      _person =  newPerson;
+    }
+    public string GetPerson()
+    {
+      return _person;
+    }
+
+
+    public void SetPluralNoun(string newPluralNoun)
+    {
+      _pluralNoun =  newPluralNoun;
+    }
+    public string GetPluralNoun()
+    {
+      return _pluralNoun;
+    }
+
+
+    public void SetNoun1(string newNoun)
+    {
+      _noun1 =  newNoun;
+    }
+    public string GetNoun1()
+    {
+      return _noun1;
+    }
+
+
+    public void SetNoun2(string newerNoun)
+    {
+      _noun2 =  newerNoun;
+    }
+    public string GetNoun2()
+    {
+    return _noun2;
+    }
+
+
+    public void SetVerb(string newVerb)
+    {
+      _verb =  newVerb;
+    }
+    public string GetVerb()
+    {
+    return _verb;
+    }
+
+    public void SetVegetable(string newVegetable)
+    {
+      _vegetable =  newVegetable;
+    }
+    public string GetVegetable()
+    {
+      return _vegetable;
+    }
+
+    public void SetColor(string newColor)
+    {
+      _color =  newColor;
+    }
+    public string GetColor()
+    {
+      return _color;
+    }
+
+    public void SetNoun3(string newestNoun)
+    {
+      _noun3 =  newestNoun;
+    }
+    public string GetNoun3()
+    {
+      return _noun3;
+    }
+
+  }
+}
